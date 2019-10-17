650a0f8c04627cee48fb1c4993a06e89c24cf3e3
dwayhs <daniel.wayhs@toptal.com>
2019-10-17 18:53:42 -0300

    server routes to list and create index

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,32 @@
 export default function (app) {
+  const searchEngine = app.get('searchEngine')
+
   app.get('/', function(req, res) {
     res
       .status(200)
       .send({
         name: 'Search engine playground',
+        searchEngine: {
+          version: searchEngine.version,
+        }
+      })
+  })
+
+  app.post('/:indexName', function(req, res) {
+    searchEngine.indices.create(req.params.indexName)
+
+    res
+      .status(201)
+      .send({
+        message: 'Index created'
+      })
+  })
+
+  app.get('/_indices', function(req, res) {
+    res
+      .status(200)
+      .send({
+        indices: searchEngine.indices.all,
       })
   })
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import routes from './routes/routes'
+import SearchEngine from 'search-engine'
 
 const app = express()
+
+const searchEngine = new SearchEngine()
+
+app.set('searchEngine', searchEngine)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
