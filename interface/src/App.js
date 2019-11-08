097d42a270e73dbcf3c95513ac757a8fa45ebe1c
dwayhs <daniel.wayhs@toptal.com>
2019-11-08 17:21:44 -0300

    Fix incorrect import

--- interface/src/App.js
+++ interface/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Structure from './components/structure/Structure'
 import Playground from './pages/Playground'
 import './App.css'
-import 'antd/dist/antd.css'
 
 export default () => {
   return (
